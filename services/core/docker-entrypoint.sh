0dbdbcdd160738722ecd0ee3d16d7ec12fecd9e7
Neo <neo@ai-my-time.local>
2026-08-27 07:33:06 +0000

    ops: add isolated production deployment contour

---
+++ services/core/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+for setting in DATABASE_URL TELEGRAM_BOT_TOKEN TELEGRAM_LEAD_WEBHOOK_SECRET; do
+    file_var="${setting}_FILE"
+    eval "secret_file=\${$file_var:-}"
+    if [ -n "$secret_file" ]; then
+        test -r "$secret_file"
+        value=$(cat "$secret_file")
+        test -n "$value"
+        export "$setting=$value"
+    fi
+done
+
+exec "$@"
